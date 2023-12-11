f30421790a674c65566528546f03c16d0f5618a7
ZiadAhmed92 <za693387@gmail.com>
2023-12-11 23:54:55 +0200

    edit design

--- src/pages/message.js
+++ src/pages/message.js
@@ -3,57 +3,55 @@ import Image from "next/image"
 import { useContext, useEffect, useState } from "react"
 import { CountContext } from "../Context/Store"
 import axios from "axios"
-
 import Link from "next/link"
 import img1 from "../image/img1.jpg"
 const Message = () => {
-    let [url, setUrl] = useState("");
-    let [Messages, setMessages] = useState([]);
-    let {res,userData,dataUser} = useContext(CountContext)
+  let [url, setUrl] = useState("");
+  let [Messages, setMessages] = useState([]);
+  let { res, userData, dataUser } = useContext(CountContext)
 
-   
-    async function getMessages() {
-      try {
-        let { data } = await axios.get("https://saraha874.onrender.com/message",{ headers:{
+
+  async function getMessages() {
+    try {
+      let { data } = await axios.get("https://saraha874.onrender.com/message", {
+        headers: {
           'token': `${localStorage.getItem("token")}`,
-  
-        }});
-        setMessages(data.messages)
-       
-     
-      } catch (err) {
-        console.log(err)
-      }
-  
+        }
+      });
+      setMessages(data.messages)
+    } catch (err) {
+      console.log(err)
     }
-    useEffect(() => {
-      dataUser();
-      getMessages()
-      
-    }, [])
-  
-     function getUrl(){
-      // https://sarahaa.vercel.app/
-      setUrl(`https://sarahaa.vercel.app/${userData?.userId}`)
-      
-     }
+
+  }
+  useEffect(() => {
+    dataUser();
+    getMessages()
+
+  }, [])
+
+  function getUrl() {
+    // https://sarahaa.vercel.app/
+    setUrl(`https://sarahaa.vercel.app/${userData?.userId}`)
+
+  }
   return (
-<>
-    <div className="message-main">
-    <div className="m-auto bg-info">
-      <Image src={img1} width={100} height={100} alt="img" />
-    </div>
-    <div className="message-area">
-      <h2>{userData?.name}</h2>
-    </div>
-    <button className="mb-2 btn-send" onClick={getUrl} ><i className=" text-info fa-solid fa-share-nodes"></i> Share Profile </button>
-    {url?<Link   href={{
-              pathname: `${url}`,
-              query:{name:`${userData?.name}`} ,
-            }} className='w-75 url p-2 alert alert-primary text-danger'>{url}</Link>:""}
-    {Messages.map((mess,i)=><p key={i} className='w-75  p-2 alert alert-primary text-danger'>{mess.message}</p>)}
-  </div>
-  </>
+    <>
+      <div className="message-main">
+        <div className="m-auto bg-info">
+          <Image src={img1} width={100} height={100} alt="img" />
+        </div>
+        <div className="message-area">
+          <h2>{userData?.name}</h2>
+        </div>
+        <button className="mb-2 btn-send" onClick={getUrl} ><i className=" text-info fa-solid fa-share-nodes"></i> Share Profile </button>
+        {url ? <Link href={{
+          pathname: `${url}`,
+          query: { name: `${userData?.name}` },
+        }} className='w-75 url p-2 alert alert-primary text-danger'>{url}</Link> : ""}
+        {Messages.map((mess, i) => <p key={i} className='w-75 url p-2 alert alert-primary text-dark'>{mess.message}</p>)}
+      </div>
+    </>
   )
 }
 
